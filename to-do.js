b27f0fe40cdc09a7cc67bc7ed303b593f607f987
StephenFitz11"git config --global user.email smf0211@gmail.comStephenFitz11git config --global user.name 'StephenFitz11 <smf0211@gmail.com>
2019-12-19 20:47:52 -0600

    added to dod

--- to-do.js
+++ to-do.js
@@ -1,8 +1,13 @@
+//TODO: App Wide
+//TODO: Implement expiring login tokens
+
 //TODO: Login Page
 //TODO: If login redirect to dashboard
 
+//TODO: Link the avatar to the profile page
+
 //TODO: Dashboard Page
-//
+//TODO: Make a funtioning dashboard
 
 //TODO: Client's Page
 //TODO: Make the delete move the client to "deleted clients" ability to restore clients for 30 days.
@@ -15,8 +20,17 @@
 //TODO: Edit employee Drawer: Make client's names appear on Client's entry rather than the id#
 //TODO: Edit drawer "Update" button is broken if new client is added. fix.
 //TODO: Create phone number sanitaztion and formatting
+//TODO: Display start date on info as xx-xx-xxxx format
 
-//TODO: Implement expiring login tokens
-//TODO: Make a funtioning dashboard
+//TODO: Manage Time Page
+//TODO: Edit time as admin doesn't work
+//TODO: Calendar filter doesn't work
+//TODO: Admin should be able to add time for all employees and self.
+//TODO: Add time drawer for admin is broke.
+//TODO: Display time in x.xx format
+
+//TODO:Invoices Page:
+//TODO: Make invoices page
+
+//TODO: Account Page
 //TODO: make a profile page
-//TODO: Link the avatar to the profile page
